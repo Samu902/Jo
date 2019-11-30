a9d4b27b4c2a51be883cee6c7bd24ab94370a0c5
Samu902 <samuelemarrani@gmail.com>
2019-11-30 12:11:19 +0100

    Rifinitura GameLoop (con vecchie alternative)

--- Jo/Assets/Scripts/GameManager.cs
+++ Jo/Assets/Scripts/GameManager.cs
@@ -20,7 +20,7 @@ public class GameManager : MonoBehaviour
     private GameObject[] initialPlayers;
     private List<GameObject> players;
 
-    void Start()
+    private void Start()
     {
         instance = this;
 
@@ -32,37 +32,140 @@ public class GameManager : MonoBehaviour
         StartCoroutine(GameLoop());
     }
 
-    void Update()
+    private void Update()
     {
-        UpdateTime();
+        //UpdateTime();
     }
 
-    IEnumerator GameLoop()
+    private IEnumerator GameLoop()
     {
-        //Ci si "paracaduta" nell'arena
+        ////Ci si "paracaduta" nell'arena
 
-        while (players.Count > 1)
-        {
-            while (turns % turnsToRestrict != 0 || turns == 0)
-            {
-                turns++;
+        //while (players.Count > 1)
+        //{
+        //    while (turns % turnsToRestrict != 0 || turns == 0)
+        //    {
+        //        turns++;
 
-                //15s per decidere la mossa, nel caso non si metta nulla si resta fermi
-                remainingTime = cronoTime;
-                while (remainingTime > 0)
-                {
-                    yield return new WaitForEndOfFrame();
-                }
-                Debug.Log("Tutti hanno segnato la mossa/è finito il tempo");
-                //Tutti fanno la mossa contemporaneamente
-                Debug.Log("Tutti hanno fatto la loro mossa e abbiamo visto cosa è successo all'ambiente");
-                //Si muove l'ambiente    
-            }
-            //Se sono passati X turni si restringe l'area
-            Debug.LogFormat("[Turno {0}] Si è ristretta l'area!", turns);
-            break;
-        }
+        //        //15s per decidere la mossa, nel caso non si metta nulla si resta fermi
+        //        remainingTime = cronoTime;
+        //        while (remainingTime > 0)
+        //        {
+        //            yield return new WaitForEndOfFrame();
+        //        }
+        //        Debug.Log("Tutti hanno segnato la mossa/è finito il tempo");
+
+        //        //Tutti fanno la mossa contemporaneamente
+        //        Debug.Log("Tutti hanno fatto la loro mossa");
+        //        if (Random.value >= 0.75f)
+        //        {
+        //            int r = Random.Range(0, players.Count - 1);
+        //            players.RemoveAt(r);
+        //            Debug.Log("Giocatore " + (r + 1) + " eliminato");
+        //        }
+
+        //        //Si muove l'ambiente    
+        //        Debug.Log("Abbiamo visto cosa è successo all'ambiente");
+        //    }
+        //    //Se sono passati X turni si restringe l'area
+        //    Debug.LogFormat("[Turno {0}] Si è ristretta l'area!", turns);
+        //}
+        ////Si riparte e si continua finché non ne rimane 1
+
+        ////Roba vittoria
+
+
+        ////Ci si "paracaduta" nell'arena
+        //do
+        //{
+        //    do
+        //    {
+        //        turns++;
+
+        //        //15s per decidere la mossa, nel caso non si metta nulla si resta fermi
+        //        remainingTime = cronoTime;
+        //        while (remainingTime > 0)
+        //        {
+        //            yield return new WaitForEndOfFrame();
+        //        }
+        //        Debug.Log("Tutti hanno segnato la mossa/è finito il tempo");
+
+        //        //Tutti fanno la mossa contemporaneamente
+        //        Debug.Log("Tutti hanno fatto la loro mossa");
+        //        if (Random.value >= 0.75f)
+        //        {
+        //            int r = Random.Range(0, players.Count - 1);
+        //            players.RemoveAt(r);
+        //            Debug.Log("Giocatore " + (r + 1) + " eliminato");
+
+        //            if (players.Count <= 1)
+        //                Debug.Log("E' rimasto solo 1 giocatore");
+        //        }
+
+        //        //Si muove l'ambiente    
+        //        Debug.Log("Abbiamo visto cosa è successo all'ambiente");
+        //    }
+        //    while (turns % turnsToRestrict != 0);
+
+        //    //Quando sono passati X turni si restringe l'area
+        //    Debug.LogFormat("[Turno {0}] Si è ristretta l'area!", turns);
+        //}
+        //while (players.Count > 1);
         //Si riparte e si continua finché non ne rimane 1
+
+        //Roba vittoria
+
+
+        //Ci si "paracaduta" nell'arena
+        do
+        {
+            turns++;
+            Debug.Log("[Turno " + turns + "]");
+
+            //15s per decidere la mossa, nel caso non si metta nulla si resta fermi
+            remainingTime = cronoTime;
+            while (remainingTime > 0)
+            {
+                remainingTime -= Time.deltaTime;
+                crono.text = Mathf.Round(remainingTime) < 10 ? "0: 0" + Mathf.Round(remainingTime) : "0: " + Mathf.Round(remainingTime);
+                yield return new WaitForEndOfFrame();
+            }
+            Debug.Log("Tutti hanno segnato la mossa/è finito il tempo");
+
+            //Tutti fanno la mossa contemporaneamente
+            yield return new WaitForSeconds(0.2f);
+            Debug.Log("Tutti hanno fatto la loro mossa");
+            if (Random.value <= 0.75f)
+            {
+                int r = Random.Range(0, players.Count - 1);
+                players.RemoveAt(r);
+                Debug.Log("Giocatore " + (r + 1) + " eliminato");
+
+                if (players.Count <= 1)
+                {
+                    yield return new WaitForSeconds(0.2f);
+                    Debug.Log("E' rimasto solo 1 giocatore");
+                    break;
+                }
+            }
+
+            //Si muove l'ambiente    
+            yield return new WaitForSeconds(0.2f);
+            Debug.Log("Abbiamo visto cosa è successo all'ambiente");
+
+            if(turns % turnsToRestrict == 0)
+            {
+                //Quando sono passati X turni si restringe l'area
+                yield return new WaitForSeconds(0.2f);
+                Debug.Log("Si è ristretta l'area!");
+            }
+        }
+        while (players.Count > 1);
+        //Si riparte e si continua finché non ne rimane 1
+
+        //Roba vittoria
+        yield return new WaitForSeconds(0.2f);
+        Debug.Log("Hai vinto!!!");
     }
 
     private void UpdateTime()
